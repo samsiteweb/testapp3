2f28857c1bc3d8628c277343f9675f566914dfb4
Lawal Samuel Olamide <samthedonz@gmail>
2018-03-05 00:48:53 -0800

    edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TabsPage } from '../pages/tabs/tabs';
 
-// import { TabsPage } from '../pages/tabs/tabs';
+
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = 'ItemsPage';
+  rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,6 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireOfflineModule } from 'angularfire2-offline';
-import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
-
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyArgwrfPjhrRI2Tv_5rwINR_JW2ceUY_JY",
-  authDomain: "studentplannerapp.firebaseapp.com",
-  databaseURL: "https://studentplannerapp.firebaseio.com",
-  projectId: "studentplannerapp",
-  storageBucket: "studentplannerapp.appspot.com",
-  messagingSenderId: "266581502180"
-};
 
 @NgModule({
   declarations: [
@@ -35,9 +22,6 @@ export const firebaseConfig = {
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule,
-    AngularFireOfflineModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
